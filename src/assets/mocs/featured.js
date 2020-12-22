a2babbaca55173fe42628b53f213b2ce78766431
ozgeek <myozgeekz@gmail.com>
2020-12-22 12:16:59 +0200

    First commit

---
+++ src/assets/mocs/featured.js
@@ -0,0 +1,32 @@
+import moment from "moment";
+
+export default [
+  {
+    title: "Can anyone cook1 ",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "one.jpg",
+  },
+  {
+    title: "Can anyone cook2 ",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "two.jpg",
+  },
+  {
+    title: "Can anyone cook3 ",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "three.jpg",
+  },
+  {
+    title: "Can anyone cook4",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "four.jpg",
+  },
+];
---
+++ src/assets/mocs/trending.js
@@ -0,0 +1,41 @@
+import moment from "moment";
+
+const trending = [
+  {
+    title: "Can anyone cook1",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "one.jpg",
+  },
+  {
+    title: "Can anyone cook2 ",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "two.jpg",
+  },
+  {
+    title: "Can anyone cook3 ",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "three.jpg",
+  },
+  {
+    title: "Can anyone cook4",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "four.jpg",
+  },
+  {
+    title: "Can anyone cook5",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Health Culture", "Food News"],
+    link: "#",
+    image: "five.jpg",
+  },
+];
+
+export default trending;
